55e399ca7da71e0c3edccdfcdeebe7804276c557
Gianlucca Agostini <gianluccaagostini97@gmail.com>
2023-10-28 15:44:32 -0300

    Inicio dos componentes/interface/service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProdutosComponent } from './pages/produtos/produtos.component';
+import { CadastrarProdutosComponent } from './pages/cadastrar-produtos/cadastrar-produtos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ProdutosComponent,
+    CadastrarProdutosComponent
   ],
   imports: [
     BrowserModule,
