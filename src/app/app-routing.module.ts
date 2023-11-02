b55d293c24c5cdb59b0e4f7485202e7cb30c1866
Gianlucca Agostini <gianluccaagostini97@gmail.com>
2023-11-02 14:55:49 -0300

    ajustes na tela home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProdutosComponent } from './pages/produtos/produtos.component';
 import { CadastrarProdutosComponent } from './pages/cadastrar-produtos/cadastrar-produtos.component';
 import { EditarProdutosComponent } from './pages/editar-produtos/editar-produtos.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
   {
     path: 'produtos', component: ProdutosComponent
   },
